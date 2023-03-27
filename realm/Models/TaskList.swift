58069e6812858fd2819c8879aa733f634e01602a
lenalog13 <56520937+lenalog13@users.noreply.github.com>
2023-03-27 18:52:07 +0300

    model creation

--- realm/Models/TaskList.swift
+++ realm/Models/TaskList.swift
@@ -8,3 +8,16 @@
 import Foundation
 import RealmSwift
 
+
+class TaskList: Object {
+    @Persisted var name = ""
+    @Persisted var date = Date()
+    @Persisted var task = List<Task>()
+}
+
+class Task: Object {
+    @Persisted var name = ""
+    @Persisted var note = ""
+    @Persisted var date = Date()
+    @Persisted var isComlete = false
+}
